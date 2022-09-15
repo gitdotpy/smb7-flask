d01de9c9528446d5fc0d925d7bef9592ee07bc4f
Shane Brennan <shane.m.brenn@gmail.com>
2022-09-15 08:40:59 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+app = Flask(__name__)
+# database instance
+db = SQLAlchemy(app)
+# connects app file to db
+app.config['SQLALCHEMY_DATBASE_URI'] = 'sqlite:///database.db'
+# secret key for security
+app.config['SECRET_KEY'] = 'thisissecretkey'
+db.init_app(app)
+
+# create table
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
